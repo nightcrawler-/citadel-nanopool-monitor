eddd60a49a2edc820d5e422c4f3a6819c4db6bd9
Frederick Nyawaya <o.frederickn@gmail.com>
2021-04-29 01:31:31 +0300

    qe scan concerns

---
+++ app/src/main/java/com/cafrecode/citadel/ui/QrScanActivity.kt
@@ -0,0 +1,73 @@
+package com.cafrecode.citadel.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.cafrecode.citadel.databinding.ActivityQrScanBinding
+
+class QrScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQrScanBinding
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQrScanBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        codeScanner = CodeScanner(this, binding.scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_SHORT).show()
+                closeScannerAndBack(it.text)
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        binding.scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    private fun closeScannerAndBack(data: String) {
+        val data = Intent()
+        data.putExtra(HomeFragment.WALLET_ADDRESS, data)
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
