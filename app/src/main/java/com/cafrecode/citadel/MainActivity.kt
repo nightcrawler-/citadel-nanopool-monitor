0557c26e31f35889c5d55ef8337bf3ef34e6fb0b
Frederick Nyawaya <o.frederickn@gmail.com>
2021-04-29 01:04:09 +0300

    lean home fragment, prompts to add wallet

--- app/src/main/java/com/cafrecode/citadel/MainActivity.kt
+++ app/src/main/java/com/cafrecode/citadel/MainActivity.kt
@@ -18,8 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
