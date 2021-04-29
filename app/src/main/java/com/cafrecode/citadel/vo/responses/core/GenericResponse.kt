052b4e67b498202af2cc5b3d5d80e372f6eeee25
Frederick Nyawaya <o.frederickn@gmail.com>
2021-04-29 23:16:44 +0300

    typo fix

--- app/src/main/java/com/cafrecode/citadel/vo/responses/core/GenericResponse.kt
+++ app/src/main/java/com/cafrecode/citadel/vo/responses/core/GenericResponse.kt
@@ -3,7 +3,7 @@ package com.cafrecode.citadel.vo.responses.core
 data class GenericResponse(val status: Boolean, val data: String) {
 }
 
-// Extendtion functions for Generic Response. Is this the right place? Seems like it. 'data' can be oe of the below mentioned depending on the endpoint hit
+// Extension functions for Generic Response. Is this the right place? Seems like it. 'data' can be oe of the below mentioned depending on the endpoint hit
 //TODO: Revise, for locale currency too
 fun GenericResponse.balance(): String {
     return String.format("%.6f XMR", this.data.toFloat())
